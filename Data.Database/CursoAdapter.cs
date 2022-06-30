dbaf6e00a2df600c36952122e652e0fa7719b722
Ignacio <martinezignacio99@gmail.com>
2022-06-30 11:25:38 -0300

    Implementacion SQLServer en CursosAdapter
    se modifico todas las funciones en CursoAdapter para trabajar con la base de datos

--- Data.Database/CursoAdapter.cs
+++ Data.Database/CursoAdapter.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Business.Entities;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Data.Database
 {
@@ -46,41 +48,161 @@ namespace Data.Database
 
         public List<Curso> GetAll()
         {
-            return new List<Curso>(Cursos);
+            // return new List<Curso>(Cursos);
+            List<Curso> cursos = new List<Curso>();
+            try
+            {
+                this.OpenConnection();
+
+                SqlCommand cmdCursos = new SqlCommand("select * from cursos", sqlConn);
+
+                SqlDataReader drCursos = cmdCursos.ExecuteReader();
+                
+                while (drCursos.Read())
+                {
+                    Curso cur = new Curso();
+
+                    cur.AnioCalendario = (int)drCursos["anio_calendario"];
+                    cur.Cupo = (int)drCursos["cupo"];
+                    // cur.Descripcion = (string)drCursos["descripcion"];
+
+                    cursos.Add(cur);
+                }
+
+                drCursos.Close();
+                this.CloseConnection();
+
+                return cursos;
+            }
+            catch (Exception Ex) 
+            {
+                Exception ExcepcionManejada = new Exception("Error al recuperar lista de cursos", Ex);
+                throw ExcepcionManejada;
+            }
         }
 
         public Business.Entities.Curso GetOne(int ID)
         {
-            return Cursos.Find(delegate (Curso c) { return c.ID == ID; });
+            // return Cursos.Find(delegate (Curso c) { return c.ID == ID; });
+            Curso curso = new Curso();
+
+            try
+            {
+                this.OpenConnection();
+
+                SqlCommand cmdCurso = new SqlCommand("select * from cursos where id_curso = @id",sqlConn);
+
+                cmdCurso.Parameters.Add("@id", SqlDbType.Int).Value = ID;
+
+                SqlDataReader drCurso = cmdCurso.ExecuteReader();
+
+                if (drCurso.Read())
+                {
+                    curso.AnioCalendario = (int)drCurso["anio_calendario"];
+                    curso.Cupo = (int)drCurso["cupo"];
+                }
+                drCurso.Close();
+            }
+
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al recuperar datos del curso", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+            return curso;
         }
 
         public void Delete(int ID)
         {
-            Cursos.Remove(this.GetOne(ID));
+            //Cursos.Remove(this.GetOne(ID));
+            try
+            {
+                this.OpenConnection();
+
+                SqlCommand cmdDelete = new SqlCommand("delete cursos where id_curso=@id", sqlConn);
+                cmdDelete.Parameters.Add("@id", SqlDbType.Int).Value = ID;
+
+                cmdDelete.ExecuteNonQuery();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al eliminar curso", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
         }
+
+        public void Update(Curso curso)
+        {
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdSave = new SqlCommand("UPDATE cursos SET anio_calendario = @anio_calendario, cupo = @cupo" +
+                "WHERE id_curso=@id", sqlConn);
+                
+                cmdSave.Parameters.Add("@id", SqlDbType.Int).Value = curso.ID;
+                cmdSave.Parameters.Add("@anio_calendario", SqlDbType.Int).Value = curso.AnioCalendario;
+                cmdSave.Parameters.Add("@cupo", SqlDbType.Int).Value = curso.Cupo;
+                cmdSave.ExecuteNonQuery();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al modificar datos del curso", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+
+        public void Insert(Curso curso)
+        {
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdSave = new SqlCommand("insert into cursos(anio_calendario,cupo)" + 
+                 "values (@anio_calendario, @cupo)" + 
+                  "select @@identity", sqlConn);
+                cmdSave.Parameters.Add("anio_calendario", SqlDbType.Int).Value = curso.AnioCalendario;
+                cmdSave.Parameters.Add("cupo", SqlDbType.Int).Value = curso.Cupo;
+                curso.ID = decimal.ToInt32((decimal)cmdSave.ExecuteScalar());
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al crear curso", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+
+        }
+
 
         public void Save(Curso curso)
         {
-            if (curso.State == BusinessEntity.States.New)
-            {
-                int NextID = 0;
-                foreach (Curso cur in Cursos)
-                {
-                    if (cur.ID > NextID)
-                    {
-                        NextID = cur.ID;
-                    }
-                }
-                curso.ID = NextID + 1;
-                Cursos.Add(curso);
-            }
-            else if (curso.State == BusinessEntity.States.Deleted)
+            if(curso.State == BusinessEntity.States.Deleted)
             {
                 this.Delete(curso.ID);
             }
+
+            else if (curso.State == BusinessEntity.States.New)
+            {
+                this.Insert(curso);
+            }
+
             else if (curso.State == BusinessEntity.States.Modified)
             {
-                Cursos[Cursos.FindIndex(delegate (Curso cur) { return cur.ID == curso.ID; })] = curso;
+                this.Update(curso);
             }
             curso.State = BusinessEntity.States.Unmodified;
         }
